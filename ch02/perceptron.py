14494eee6da392e7acf7264d4232488e7a5aaba4
Naoki Ishikawa <naokiishikawa0808@gmail.com>
2016-12-14 08:56:44 +0900

    implemented OR gate

--- ch02/perceptron.py
+++ ch02/perceptron.py
@@ -29,3 +29,18 @@ print(NAND(0, 0))
 print(NAND(1, 0))
 print(NAND(0, 1))
 print(NAND(1, 1))
+
+def OR(x1, x2):
+    x = np.array([x1, x2])
+    w = np.array([0.5, 0.5])
+    b = 0.0
+    tmp = np.sum(w*x) + b
+    if tmp <= 0:
+        return 0
+    else:
+        return 1
+
+print(OR(0, 0))
+print(OR(1, 0))
+print(OR(0, 1))
+print(OR(1, 1))
