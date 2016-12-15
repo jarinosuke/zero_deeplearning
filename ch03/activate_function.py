076998beca793361e1ac8fc6e97a44d1e261a3eb
Naoki Ishikawa <naokiishikawa0808@gmail.com>
2016-12-16 08:32:08 +0900

    created sigmoid function

--- ch03/activate_function.py
+++ ch03/activate_function.py
@@ -12,3 +12,13 @@ y = step_function(x)
 plt.plot(x, y)
 plt.ylim(-0.1, 1.1)
 plt.show()
+
+def sigmoid(x):
+    return 1 / (1 + np.exp(-x))
+
+x1 = np.arange(-5.0, 5.0, 0.1)
+y1 = sigmoid(x)
+
+plt.plot(x1, y1)
+plt.ylim(-0.1, 1.1)
+plt.show()
