8ad5863732b9e2ccb3884fd34b9d7c7e67731efa
Naoki Ishikawa <naokiishikawa0808@gmail.com>
2016-12-26 13:40:27 +0900

    implemented gradient descent

--- ch04/diff.py
+++ ch04/diff.py
@@ -33,3 +33,17 @@ plt.show()
 
 print(numerical_diff(function_1, 5))
 print(numerical_diff(function_1, 10))
+
+def gradient_descent(f, init_x, lr=0.01, step_num=100):
+    x = init_x
+
+    for i in range(step_num):
+        grad = numerical_gradient(f, x)
+        x -= lr * grad
+    return x
+
+def function_2(x):
+    return x[0]**2 + x[1]**2
+
+init_x = np.array([3.0, 4.0])
+print(gradient_descent(function_2, init_x=init_x, lr=0.1, step_num=100))
