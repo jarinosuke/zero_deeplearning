371bb3c64ee32d682f10cbf97b7a5cd23b2a4c4e
Naoki Ishikawa <naokiishikawa0808@gmail.com>
2016-12-27 10:32:37 +0900

    created sigmoid layer

--- ch05/layer.py
+++ ch05/layer.py
@@ -41,3 +41,16 @@ class Relu:
         dout[self.mask] = 0
         dx = dout
         return dx
+
+class Sigmoid:
+    def __init__(self):
+        self.out = None
+
+    def forward(self, x):
+        out = 1 / (1 + np.exp(-x))
+        self.out = out
+        return out
+
+    def backward(self, dout):
+        dx = dout * (1.0 - self.out) * self.out
+        return dx
