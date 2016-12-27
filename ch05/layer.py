8490c76719862283825d89bd497ea1f92ea7a266
Naoki Ishikawa <naokiishikawa0808@gmail.com>
2016-12-27 10:00:53 +0900

    created ReLU layer

--- ch05/layer.py
+++ ch05/layer.py
@@ -26,3 +26,18 @@ class AddLayer:
         dx = dout * 1
         dy = dout * 1
         return dx, dy
+
+class Relu:
+    def __init__(self):
+        self.mask = None
+
+    def forward(self, x):
+        self.mask = (x <= 0)
+        out = x.copy()
+        out[self.mask] = 0
+        return out
+
+    def backward(self, dout):
+        dout[self.mask] = 0
+        dx = dout
+        return dx
